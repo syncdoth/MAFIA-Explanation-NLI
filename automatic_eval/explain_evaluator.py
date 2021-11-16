682ac7f09870cb461d934490aeb3155af4147df0
syncdoth <choisehyun98@gmail.com>
2021-11-16 19:20:06 +0800

    start automatic eval

---
+++ automatic_eval/explain_evaluator.py
@@ -0,0 +1,135 @@
+import torch
+from torch import nn
+from transformers import AutoModel, AutoModelForSequenceClassification
+
+from loss import length_regularizer, continuity_regularizer
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, model_name):
+        super().__init__()
+        self.model = AutoModel.from_pretrained(model_name)
+        self.head = nn.Linear(self.model.config.hidden_size, 1)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        out = self.model(input_ids=input_ids,
+                         attention_mask=attention_mask,
+                         token_type_ids=token_type_ids).last_hidden_state  # [N, T, E]
+        return torch.sigmoid(self.head(out).squeeze(-1))  # [N, T]
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, model_name, mask_id=103):
+        super().__init__()
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name,
+                                                                        num_labels=3)
+        self.mask_id = mask_id
+
+    def forward(self, **selected_inputs):
+        """
+        inputs:
+            input_ids: N, T
+            attention_mask: N, T
+            token_type_ids: N, T
+        """
+        return self.model(**selected_inputs).logits
+
+
+class VerificationNetwork(nn.Module):
+
+    def __init__(self,
+                 model_name,
+                 mask_id=103,
+                 mask_threshold=0.5,
+                 relevance_threshold=0.1,
+                 reg_strengths=(2e-4, 4e-4)):
+        super().__init__()
+        self.encoder = Encoder(model_name)
+        self.decoder = Decoder(model_name, mask_id=mask_id)
+        self.mask_threshold = mask_threshold
+
+        self.score_cache = None
+
+        self.loss = nn.CrossEntropyLoss(reduction='none')
+        self.reg_strengths = reg_strengths
+        self.relevance_threshold = relevance_threshold
+
+    def select_inputs(self, input_ids, attention_mask, token_type_ids, selection_mask):
+        """
+        slection_mask: N, T. Binary mask of 1 and 0
+
+        NOTE: do not process token_type_ids here: no need actually.
+        """
+        # do not change [PAD] to [MASK]
+        input_ids = torch.where((selection_mask == 0) & (attention_mask == 1),
+                                self.mask_id, input_ids)
+        # mask the [MASK] tokens from attention.
+        attention_mask = attention_mask * selection_mask
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'token_type_ids': token_type_ids,
+        }
+
+    def forward(self, input_ids, attention_mask, token_type_ids, token_only=False):
+        selection_score = self.encoder(input_ids, attention_mask, token_type_ids)
+        selection_mask = selection_score >= self.threshold
+        self.score_cache = selection_score
+        selected_inputs = self.select_inputs(input_ids, attention_mask, token_type_ids,
+                                             selection_mask)
+        if token_only:
+            return selected_inputs
+
+        scores = self.decoder(**selected_inputs)
+        return scores, selected_inputs
+
+    def backward(self, x, y):
+        # loss_term.shape == [N,]
+        ce_loss = self.loss(x, y)
+        reg_loss = (self.reg_strengths[0] * length_regularizer(self.score_cache) +
+                    self.reg_strengths[1] * continuity_regularizer(self.score_cache))
+        loss_term = ce_loss + reg_loss
+        loss_term = loss_term.mean()
+
+        # decoder backward
+        loss_term.backward(retain_graph=True)
+
+        # encoer backward
+        log_scores = torch.log(self.score_cache)  # [N, T]
+        encoder_loss = (log_scores * loss_term).sum(-1).mean(0)
+        encoder_loss.backward()
+
+    def infer(self, **inputs):
+        """
+        return: [N, T, 3].
+            0 - not relevant, 1 - clearly relevant, 2 - selected don't know
+
+        TODO: find clearly relevant tokens
+        """
+        selected_inputs = self(**inputs, token_only=True)  # [N, T]
+        raise NotImplementedError
+
+
+class ExplanationEvaluator:
+
+    def __init__(self, explainer, verification_net):
+        self.explainer = explainer
+        self.verification_net = verification_net
+
+    def verify(self, inputs):
+        # TODO: implement metric 2 and 3
+        explanation = self.explainer(**inputs)  # [N, T]
+        top1 = explanation.argmax(-1)  # [N,]
+
+        tok_distribution = self.verification_net.infer(**inputs)  # [N, T]
+
+        # eval metric 1: amount of the most important token is found in Nx,
+        # the set of guaranteed not important tokens.
+        wrong_top1_percent = (tok_distribution[range(tok_distribution.shape[0]),
+                                               top1] == 0).long().mean()
+
+        # eval metric 2:
+
+        # eval metric 3:
---
+++ automatic_eval/loss.py
@@ -0,0 +1,22 @@
+import torch
+from torch.nn import CrossEntropyLoss
+
+loss = CrossEntropyLoss()
+
+
+def length_regularizer(z):
+    """
+    z = binary tensor of shape N, T
+
+    return: loss of shape N,
+    """
+    return torch.norm(z, dim=-1)
+
+
+def continuity_regularizer(z):
+    """
+    z = binary tensor of shape N, T
+
+    return: loss of shape N,
+    """
+    return (z[:, 1:] - z[:, :-1]).abs().sum(-1)
