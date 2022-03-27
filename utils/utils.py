f8dddcb6633265386f925982662abcaa2c407d12
syncdoth <choisehyun98@gmail.com>
2022-03-28 00:29:09 +0800

    load config logic with absolute path to config

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,11 @@
+import os
 import json
 
+DEFAULT_CONFIG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..',
+                                   'configs/pretrained_models.json')
 
-def load_pretrained_config(model_name, config_path='configs/pretrained_models.json'):
+
+def load_pretrained_config(model_name, config_path=DEFAULT_CONFIG_PATH):
     with open(config_path, 'r') as f:
         config = json.load(f)
     if model_name not in config:
