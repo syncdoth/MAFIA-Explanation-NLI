04a8b30ac2b34cc46bb13ce9142c1229d18d3b61
syncdoth <choisehyun98@gmail.com>
2022-04-27 04:21:16 +0800

    use python unittest

--- tests/test_evaluate_explanation.py
+++ tests/test_evaluate_explanation.py
@@ -1,27 +1,34 @@
+import sys
+
+sys.path.insert(0, '/data/schoiaj/repos/nli_explain')
+
 from ground_truth_eval.evaluate_explanation import find_common_tokens
+import unittest
 
 
-def test_find_common_tokens():
-    pred_list = [
-        ['man', 'walks', 'man'],
-        ['man', 'man'],
-        [],
-    ]
-    gt_list = [
-        ['the', 'man', 'walks'],
-        ['the', 'man', 'walks', 'man'],
-        ['none'],
-    ]
-    answers = [
-        ['man', 'walks'],
-        ['man', 'man1'],
-        [],
-    ]
+class TestEvaluation(unittest.TestCase):
 
-    for i, (p, g, a) in enumerate(zip(pred_list, gt_list, answers)):
-        common = find_common_tokens(p, g)
-        print(f'test #{i}:', 'passed' if set(a) == common else 'failed')
+    def test_find_common_tokens(self):
+        pred_list = [
+            ['man', 'walks', 'man'],
+            ['man', 'man'],
+            [],
+        ]
+        gt_list = [
+            ['the', 'man', 'walks'],
+            ['the', 'man', 'walks', 'man'],
+            ['none'],
+        ]
+        answers = [
+            ['man', 'walks'],
+            ['man', 'man1'],
+            [],
+        ]
+
+        for p, g, a in zip(pred_list, gt_list, answers):
+            common = find_common_tokens(p, g)
+            self.assertEqual(set(a), common)
 
 
 if __name__ == '__main__':
-    test_find_common_tokens()
+    unittest.main()
